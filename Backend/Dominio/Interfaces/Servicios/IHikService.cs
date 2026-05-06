5cd4877212af35f9c9e0d02857942c1e5219df79
NicolasMarai <nicolasmarai@hotmail.com>
2026-05-06 19:46:43 -0300

    Agrego Integración Hik patron Strategy

---
+++ Backend/Dominio/Interfaces/Servicios/IHikService.cs
@@ -0,0 +1,7 @@
+﻿namespace ProyectoFinal_Grupo6.Api.Dominio.Interfaces.Servicios
+{
+    public interface IHikService
+    {
+
+    }
+}
---
+++ Backend/Infraestructura/Servicios/HikService.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoFinal_Grupo6.Api.Dominio.Interfaces.Servicios;
+
+namespace ProyectoFinal_Grupo6.Api.Infraestructura.Servicios
+{
+    public class HikService : IHikService
+    {
+        public IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+        public HikService(HttpClient httpClient, IConfiguration configuration)
+        {
+            this._httpClient = httpClient;
+            this._configuration = configuration;
+        }
+
+
+    }
+}
